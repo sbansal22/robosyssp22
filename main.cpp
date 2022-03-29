45e83862343eb4e24c97c5f9744d597da452a7e5
Sparsh Bansal <sbansal@olin.edu>
2022-03-29 14:11:13 -0400

    Test+fix turning code - Maya

--- main.cpp
+++ main.cpp
@@ -11,44 +11,49 @@ void setup()
        rmotor.attach(10);
 
        pinMode(6, INPUT);
-       pinMode(7, INPUT);
+       pinMode(5, INPUT);
 }
 
 void loop()
 {
 
        ch1 = pulseIn(6, HIGH);
-       ch2 = pulseIn(7, HIGH);
+       ch2 = pulseIn(5, HIGH);
 
+       // Going backwards
        if (ch1 >= 1500)
        {
+              // turning right
               if (ch2 >= 1500)
               {
-                     lmotor.write(120);
-                     rmotor.write(60);
+                     lmotor.write(80);
+                     rmotor.write(65);
               }
               else if (ch2 <= 1450)
               {
-                     lmotor.write(60);
-                     rmotor.write(120);
+                     lmotor.write(65);
+                     rmotor.write(80);
               }
+              // Straight backward
               else
               {
                      lmotor.write(60);
                      rmotor.write(60);
               }
        }
+
+       // Straight backward
        else if (ch1 <= 1450)
        {
               if (ch2 >= 1500)
               {
-                     lmotor.write(120);
-                     rmotor.write(60);
+                     lmotor.write(100);
+                     rmotor.write(115);
               }
               else if (ch2 <= 1450)
               {
-                     lmotor.write(60);
-                     rmotor.write(120);
+                     lmotor.write(115);
+                     rmotor.write(100);
               }
               else
               {
